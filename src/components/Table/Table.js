073aae638183bd35530271787efb255f7c37ff72
m7real <masayeedreal@gmail.com>
2022-12-11 02:38:07 +0600

    basic table added

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -11,23 +11,36 @@ const Table = () => {
   }, []);
 
   return (
-    <div>
-      <table>
-        <tr>
-          <th>Company</th>
-          <th>Contact</th>
-          <th>Country</th>
-        </tr>
-        <tr>
-          <td>Alfreds Futterkiste</td>
-          <td>Maria Anders</td>
-          <td>Germany</td>
-        </tr>
-        <tr>
-          <td>Centro comercial Moctezuma</td>
-          <td>Francisco Chang</td>
-          <td>Mexico</td>
-        </tr>
+    <div className=" h-[95vh] overflow-scroll">
+      <table className="table table-compact w-full">
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Gender</th>
+            <th>IP Address</th>
+            <th>Airport Code</th>
+            <th>Date</th>
+            <th>Mobile</th>
+            <th>Area</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((person) => (
+            <tr>
+              <td>{person.first_name}</td>
+              <td>{person.last_name}</td>
+              <td>{person.email}</td>
+              <td>{person.gender}</td>
+              <td>{person.ip_address}</td>
+              <td>{person.airport_code}</td>
+              <td>{person.time}</td>
+              <td>{person.mobile}</td>
+              <td>{person.area}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
